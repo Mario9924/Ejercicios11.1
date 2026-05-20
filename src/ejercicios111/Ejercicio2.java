e0d4ed702438b6b4798fdae42482e90f3d83c61b
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-05-21 00:06:59 +0200

    Se realiza el ejercicio2 y se añaden comentarios a la plantilla

--- src/ejercicios111/Ejercicio2.java
+++ src/ejercicios111/Ejercicio2.java
@@ -1,39 +1,41 @@
-
 package ejercicios111;
-
 import java.sql.*;
 
 /**
- *
+ * 2- Genera 10 bases de datos que se llamen bd1,bd2?  Hazlo de forma recursiva (con un bucle). 
+ *  Ha de contabilizarse cuántas acciones se han hecho en total.
+ * 
  * @author Mario Gutiérrez
- * @see
+ * @see https://classroom.google.com/u/1/c/ODA3NDY5OTY5MTcz/a/ODY0OTY0NTU5Mjg0/details
  * @version 1.0
  */
-public class Plantilla {
+public class Ejercicio2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declaración de variables
-        String url = "jdbc:mysql://localhost:3307/BASE-DE-DATOS";
+        String url = "jdbc:mysql://localhost:3307/";
         String user = "root";
         String pass = "";
         try (
                 Connection conn = DriverManager.getConnection(url, user, pass);
                 Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("CONSULTA A LA BASE DE DATOS"); // USAR PARA CONSULTAS
                 
             ){
-            
+
             //se carga la clase del Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             
-            /*
-                Si lo que queremos es realizar sentencias DDL: Create - Drop - Alter - etc
-                 tenemos que usar 
-                stmt.executeUpdate("SENTENCIA DDL") ya que no devuelve un result set, solamente el número de registros modificados
-            */
+            //creamos las 10 bases de datos
+            int contadorCreaciones = 0;
+            for (int i=1; i <= 10; i++){
+                stmt.executeUpdate("create database ejdb"+i);
+                contadorCreaciones++;
+            }
+            
+            System.out.println("Se han realizado un total de " + contadorCreaciones + " creaciones.");
             
         }  catch(SQLException sqlex){
             System.err.println("Ha habido un error a la hora de trabajar con la base de datos: " + sqlex);
@@ -41,8 +43,6 @@ public class Plantilla {
         catch (Exception e) {
             System.out.println("Error: " + e);
         }
-
-
     }
     
 }
