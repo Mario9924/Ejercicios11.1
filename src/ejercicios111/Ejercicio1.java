0f98f0bbd21d0638dec48e07b627163a637cd454
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-05-24 23:45:50 +0200

    Correciones varias

--- src/ejercicios111/Ejercicio1.java
+++ src/ejercicios111/Ejercicio1.java
@@ -17,7 +17,7 @@ public class Ejercicio1 {
      */
     public static void main(String[] args) {
         // Declaración de variables
-        String url = "jdbc:mysql://localhost:3307/ejdb1";
+        String url = "jdbc:mysql://localhost:3307";
         String user = "root";
         String pass = "";
         try (
--- src/ejercicios111/Ejercicio51.java
+++ src/ejercicios111/Ejercicio51.java
@@ -26,9 +26,12 @@ public class Ejercicio51 {
         String pass = "";
         int id = 60;
         try (
-                Connection conn = DriverManager.getConnection(url, user, pass); 
-                PreparedStatement pstmt = conn.prepareStatement("INSERT INTO `personajesdb` "
-                + "() VALUES (?, ?, ?, ?, ?);");) {
+                Connection conn = DriverManager.getConnection(url, user, pass); //
+                 PreparedStatement pstmt = conn.prepareStatement("INSERT INTO `personajesdb` "
+                        + "() VALUES (?, ?, ?, ?, ?);"); // 
+                 Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);//
+                 ResultSet rs = stmt.executeQuery("Select * from personajesdb"); //
+                ) {
 
             // Introducimos datos mediante una prepared Statement
             //2- relleno las columnas que necesite
@@ -43,25 +46,25 @@ public class Ejercicio51 {
             for (int i = 0; i < 3; i++) {
                 try {
                     id++;
-                    
+
                     // Pasamos al siguiente ID correspondiente
                     pstmt.setInt(1, id);
-                    
+
                     System.out.println("Introduce el nombre del personaje");
                     pstmt.setString(2, reader.nextLine());
 
                     System.out.println("Introduce el poder del personaje");
                     pstmt.setInt(3, reader.nextInt());
-                    
+
                     reader.nextLine(); // Al pasar de un tipo de dato a otro, nuestro Scanner almacena en el buffer info extra innecesaria
                     System.out.println("Introduce la procedencia del personaje");
                     pstmt.setString(4, reader.nextLine());
 
                     System.out.println("Introduce \"0\" si el personaje está inactivo o introduce \"1\" si está activo");
                     pstmt.setInt(5, reader.nextInt());
-                    
+
                     pstmt.executeUpdate();
-                    
+
                     reader.nextLine();
                 } catch (InputMismatchException ime) {
                     System.err.println("No puedes introducir ese dato: " + ime);
@@ -69,27 +72,19 @@ public class Ejercicio51 {
                     i--;
                     id--;
                 } catch (SQLException sqlex) {
-                    System.err.println("Erro al insertar los datos : " + sqlex);
+                    System.err.println("Error al insertar los datos : " + sqlex);
+                    i--;
+                    id--;
                 }
             }
 
             // Mostramos los nuevos campos mediante una nueva conexión
             System.out.println("\nSe han añadido los siguientes personajes:\n");
-            try (
-                    Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-                    ResultSet rs = stmt.executeQuery("Select * from personajesdb");
-                ) 
-            {
-                rs.absolute(60); // sabemos que el último valor antes de las insercciones es el 60
-                while (rs.next()){
-                    System.out.println("Nombre: " + rs.getString("nombre"));
-                }
-            } catch (SQLException sqlex) {
-                System.err.println("Ha habido un error a la hora de trabajar con la base de datos: " + sqlex);
-            } catch (Exception e) {
-                System.out.println("Error: " + e);
-            }
 
+            rs.absolute(60); // sabemos que el último valor antes de las insercciones es el 60
+            while (rs.next()) {
+                System.out.println("Nombre: " + rs.getString("nombre"));
+            }
         } catch (SQLException sqlex) {
             System.err.println("Ha habido un error a la hora de trabajar con la base de datos: " + sqlex);
         } catch (Exception e) {
